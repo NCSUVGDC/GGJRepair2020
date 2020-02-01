e0070bc81e20b7e2b7f4e7a67c043dde0e27ea84
Jimmy Zheng <twzheng974@gmail.com>
2020-02-01 16:00:14 -0500

    spotlights and haze implemented

---
+++ Assets/Scripts/HazeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazeScript : MonoBehaviour
+{
+    public BoxCollider bc;
+    int count = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Spotlight"))
+        {
+            Debug.Log("spotlight collided");
+            count++;
+        }
+
+        bc.isTrigger = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Spotlight"))
+        {
+            Debug.Log("spotlight removed");
+            count--;
+        }
+
+        if (count == 0)
+        {
+            bc.isTrigger = false;
+        }
+    }
+}
