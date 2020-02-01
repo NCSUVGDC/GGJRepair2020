cf694f66ce7c6105e31778783a62332f76451687
Jimmy Zheng <twzheng974@gmail.com>
2020-02-01 10:16:59 -0500

    Created basic movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        Vector2 temp_movement;
+        temp_movement.x = 0;
+        temp_movement.y = 0;
+
+        if (Input.GetKey(moveUp))
+        {
+            temp_movement.y += 1;
+        }
+        if (Input.GetKey(moveDown))
+        {
+            temp_movement.y -= 1;
+        }
+        if (Input.GetKey(moveLeft))
+        {
+            temp_movement.x -= 1;
+        }
+        if (Input.GetKey(moveRight))
+        {
+            temp_movement.x += 1;
+        }
+
+        temp_movement.Normalize();
+
+        movement = temp_movement;
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        // Rotation
+        //rb.MoveRotation(movement);
+    }
+}
