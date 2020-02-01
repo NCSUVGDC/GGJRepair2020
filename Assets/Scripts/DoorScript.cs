6be4b3ee2002ceadd683c04cfdd4fd041bd9ee00
Jimmy Zheng <twzheng974@gmail.com>
2020-02-01 13:47:43 -0500

    restored the system

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public ButtonScript button;
+
+    public float distance = 10f;
+    public float speed = 10f;
+
+    private Vector3 endpos;
+    private Vector3 startpos;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        endpos = new Vector3(transform.position.x, transform.position.y, transform.position.z + distance);
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.pressed)
+        {
+            //Debug.Log("button pressed");
+            transform.position = Vector3.MoveTowards(transform.position, endpos, speed * Time.deltaTime);
+        } else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startpos, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SpotlightRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightRotation : MonoBehaviour
+{
+    public ButtonScript button;
+
+    public float rotationSpeed = 36f;
+    public bool counterclockwise = true;
+    
+    void FixedUpdate()
+    {
+        if (button.pressed)
+        {
+            if (counterclockwise)
+            {
+                transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime, Space.Self);
+            }
+            else 
+            {
+                transform.Rotate(0, 0, -1 * rotationSpeed * Time.fixedDeltaTime, Space.Self);
+            }
+        }
+    }
+}
