76f99fd82fee53965be2dbbab14a3009862855c2
Jimmy Zheng <twzheng974@gmail.com>
2020-02-01 20:47:20 -0500

    Added pause menu, organized scripts

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+    
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void ToMenu()
+    {
+        Debug.Log("Menu Button pressed!");
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Button pressed!");
+        Application.Quit();
+    }
+}
