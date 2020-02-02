1a2ef32256c0f7e07b5f2d4946ed44cb9c710c3b
Will <willcarpenter3@yahoo.com>
2020-02-01 23:38:46 -0500

    started win condition work

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinCondition : MonoBehaviour
 {
     int playerCount = 0;
     public bool win = false;
+
+    public string[] messages;
+    public string[] colors;
+
+    string color;
+
+    public Text redText;
+    public Text blueText;
+
+    Color purple = new Color(0.5f, 0, 1);
+    Color red = new Color(1, 0, 0);
+    Color blue = new Color(0, 0, 1);
+
+    public float timeToWait;
+
+    float elapsedTime;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,10 +31,42 @@ public class WinCondition : MonoBehaviour
             playerCount++;
         }
 
-        if (playerCount == 2)
+        if (playerCount == 2 && win == false)
         {
             Debug.Log("You win!");
             win = true;
+
+            for(int i = 0; i < messages.Length; i++)
+            {
+                Debug.Log(messages[i] + colors[i]);
+                color = colors[i];
+                if (color == "red")
+                {
+                    redText.color = red;
+                    redText.text = messages[i];
+                }
+                else if (color == "blue")
+                {
+                    blueText.color = blue;
+                    blueText.text = messages[i];
+                }
+                else if (color == "purple")
+                {
+                    redText.color = purple;
+                    blueText.color = purple;
+                    redText.text = messages[i];
+                    blueText.text = messages[i];
+                }
+
+                elapsedTime = 0;
+                while (elapsedTime < timeToWait)
+                {
+                    elapsedTime += Time.deltaTime;
+                }
+
+                redText.text = "";
+                blueText.text = "";
+            }
         }
     }
 
