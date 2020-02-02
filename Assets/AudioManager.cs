80ed8f6d1fa1b21d82a3261e2dbecebde3693757
Jimmy Zheng <twzheng974@gmail.com>
2020-02-01 20:07:33 -0500

    Audio manager and main menu implemented

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void play(string name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (string.Equals(sounds[i].name, name))
+            {
+                sounds[i].source.Play();
+                return;
+            }
+        }
+
+        Debug.LogWarning("No sound of name: " + name + " found");
+    }
+}
---
+++ Assets/WinCondition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    int playerCount = 0;
+    public bool win = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCount++;
+        }
+
+        if (playerCount == 2)
+        {
+            Debug.Log("You win!");
+            win = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!win && other.CompareTag("Player"))
+        {
+            playerCount--;
+        }
+    }
+}
